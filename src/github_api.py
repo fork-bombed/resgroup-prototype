b47cdc697ce090f15da9e64cba2f87f2fffac802
fork-bombed <ryanjmurphy2016@gmail.com>
2021-03-10 14:11:09 +0000

    p

--- src/github_api.py
+++ src/github_api.py
@@ -13,6 +13,8 @@ def get_commits_between_releases(
     ) -> [commit.Commit]:
 
     commits = []
+    print(repository.get_commits())
+    print(last_release.get_creation_time(), release.get_creation_time())
     for c in repository.get_commits():
         if (c.get_date() >= last_release.get_creation_time() and
                 c.get_date() <= release.get_creation_time()):
@@ -32,6 +34,8 @@ def format_urlsafe_time(td: timedelta) -> str:
         out.append(f'{hours}h')
     if minutes > 0:
         out.append(f'{minutes}m')
+    if len(out)==0:
+        return '0'
     return '%20'.join(out)
 
 
@@ -64,7 +68,9 @@ def get_lead_time(
             datetime.timestamp(c.get_date()) - datetime.timestamp(prev_release.get_creation_time())
             for c in commits
         ]
-        print(commits, commit_times)
+    # Stop disvision by zero
+    if len(commit_times)==0:
+        return timedelta(seconds=sum(commit_times))
     return timedelta(seconds=sum(commit_times)/len(commit_times))
 
 
@@ -74,7 +80,7 @@ def get_release_template(
         repo: repository.Repository
     ) -> str:
 
-    with open('src/templates/default.md') as file:
+    with open('ltfc/templates/default.md') as file:
         template = file.read()
     lead_time = get_lead_time(release, repo)
     if lead_time.days >= 30:
@@ -83,20 +89,26 @@ def get_release_template(
         lead_time_colour = 'important'
     else:
         lead_time_colour = 'success'
-    prev_lead_time = get_lead_time(prev_release, repo)
-    print(lead_time, prev_lead_time, lead_time - prev_lead_time, prev_lead_time - lead_time)
-    if lead_time > prev_lead_time:
-        lead_time_difference = ''.join(['+',format_urlsafe_time(lead_time - prev_lead_time)])
-        lead_time_difference_colour = 'critical'
+    if prev_release:
+        prev_lead_time = get_lead_time(prev_release, repo)
+        prev_version = prev_release.get_tage_name()
+        if lead_time > prev_lead_time:
+            lead_time_difference = ''.join(['+',format_urlsafe_time(lead_time - prev_lead_time)])
+            lead_time_difference_colour = 'critical'
+        else:
+            lead_time_difference = ''.join(['--',format_urlsafe_time(prev_lead_time - lead_time)])
+            lead_time_difference_colour = 'success'
     else:
-        lead_time_difference = ''.join(['--',format_urlsafe_time(prev_lead_time - lead_time)])
-        lead_time_difference_colour = 'success'
+        prev_version = 'start'
+        lead_time_difference = '0'
+        lead_time_difference_colour = 'yellow'
+
 
     return template.format(
         version=release.get_tag_name(),
         lead_time=format_urlsafe_time(lead_time),
         lead_time_colour=lead_time_colour,
-        prev_version=prev_release.get_tag_name(),
+        prev_version=prev_version,
         repository=repo.get_full_name(),
         lead_time_difference=lead_time_difference,
         lead_time_difference_colour=lead_time_difference_colour
@@ -115,7 +127,11 @@ if __name__ == "__main__":
     client = github.Github(token)
     repository = client.get_repository(repo)
     release = repository.get_latest_release()
-    prev_release = repository.get_releases()[1]
+    releases = repository.get_releases()
+    if len(releases) > 1:
+        prev_release = releases[1]
+    else:
+        prev_release = None
     release.update(
         message=get_release_template(
             release=release, 
